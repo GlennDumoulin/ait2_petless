4877d7a7326a21eb71176841da1c9f9d10212748
pgmgent-glendumo <glendumo@student.arteveldehs.be>
2020-10-19 22:07:45 +0200

    header & footer views added || added some responsive code || connected with db

---
+++ views/footer.php
@@ -0,0 +1,43 @@
+<footer class="footer_wrapper">
+    <div class="container">
+        <div class="footer_main row">
+            <div class="footer_info col-12 col-lg-6">
+                <div class="footer_login">
+                    <h1>Heb je al een account?</h1>
+                    <p>
+                        <a href="#">Log je hier in</a>
+                        of
+                        <a href="#">maak er nu één aan</a>
+                    </p>
+                </div>
+                <div class="footer_sitemap">
+                    <h1>Sitemap</h1>
+                    <nav class="d-flex justify-content-between">
+                        <a href="#">home</a>
+                        <a href="./dogslist.php">honden</a>
+                        <a href="#">katten</a>
+                        <a href="#">andere</a>
+                        <a href="#">dier gevonden?</a>
+                        <a href="#">contact</a>
+                    </nav>
+                </div>
+            </div>
+            <div class="footer_contact col-12 col-md-6">
+                <div class="footer_socials d-flex flex-column">
+                    <h1>Bereik ons via:</h1>
+                    <a href="tel:+3212345678">
+                        <i data-feather="phone"></i> +32 1 234 56 78
+                    </a>
+                    <a href="mailto:helpdesk.petless@gmail.com">
+                        <i data-feather="mail"></i>
+                        helpdesk.petless@gmail.com
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="footer_legal d-flex justify-content-center">
+        <p>©2020</p>
+        <p>gemaakt door Glenn Dumoulin</p>
+    </div>
+</footer>
